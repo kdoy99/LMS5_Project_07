1b7348ea6b55bc77b9b959974925c9cd680b2e49
kdy <kdoy990805@gmail.com>
2025-02-07 20:57:44 +0900

    파일

---
+++ Project07/Project07/Model/Account.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLite;
+
+namespace Project07.Model
+{
+    public class Account
+    {
+        [PrimaryKey, AutoIncrement]
+        public int UserNumber { get; set; }
+        public string ID { get; set; }
+        public string Password { get; set; }
+        public string Name { get; set; }
+        public string Age { get; set; }
+        public string Sex { get; set; }
+        public string Contact { get; set; }
+        public string Email { get; set; }
+    }
+}
